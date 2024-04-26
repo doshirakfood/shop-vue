5e3eeb94a29a449704865db15be7e30b0ff9e919
doshirakfood <doshirakfood@ya.ru>
2024-04-26 21:46:17 +0300

    #refactor: Отдельно причесал develop-страницу.

--- src/pages/develop.vue
+++ src/pages/develop.vue
@@ -2,102 +2,20 @@
 	<layout-default class="develop">
 		<develop-section title="Typography">
 			<develop-box title="Heading">
-				<h2 class="title-display">.title-display</h2>
-				<h2 v-for="index in 5" :key="index" :class="`title-${index}`">
-					{{ `.title-${index}` }}
+				<h2 class="heading heading--display">.heading-display</h2>
+				<h2
+					v-for="index in 5"
+					:key="index"
+					:class="`heading heading--${index}`"
+				>
+					{{ `.heading-${index}` }}
 				</h2>
-			</develop-box>
-
-			<develop-box title="Text">
-				<p
-					v-for="(name, index) in [
-						'tiny',
-						'small',
-						'medium',
-						'large',
-						'xl',
-						'xxl',
-					]"
-					:key="index"
-					:class="`text-${name}`"
-				>
-					{{ `.text-${name}` }}
-				</p>
-			</develop-box>
-
-			<develop-box title="Font weight">
-				<p
-					v-for="(name, index) in sizeListArray"
-					:key="index"
-					:class="`font-${name}`"
-				>
-					{{ `.font-${name}` }}
-				</p>
-			</develop-box>
-		</develop-section>
-
-		<develop-section title="Color">
-			<develop-box title="Text color">
-				<h2 class="title-6 subtitle">Gray</h2>
-				<p class="text-gray-05">.text-gray-05</p>
-				<p
-					v-for="index in 9"
-					:key="index"
-					:class="[`text-gray-${index}`]"
-				>
-					{{ `.text-gray-${index}` }}
-				</p>
-
-				<br />
-
-				<h2 class="title-6 subtitle">Green</h2>
-				<p class="text-green-05">.text-green-05</p>
-				<p
-					v-for="index in 9"
-					:key="index"
-					:class="[`text-green-${index}`]"
-				>
-					{{ `.text-green-${index}` }}
-				</p>
-			</develop-box>
-
-			<develop-box title="Background color">
-				<h2 class="title-6 subtitle">Gray</h2>
-				<div class="block bg-gray-05">.bg-gray-05</div>
-				<div
-					v-for="index in 9"
-					:key="index"
-					:class="[
-						'block',
-						`bg-gray-${index}`,
-						index >= 5 ? 'text-white' : '',
-					]"
-				>
-					{{ `.bg-gray-${index}` }}
-				</div>
-
-				<br />
-				<br />
-
-				<h2 class="title-6 subtitle">Green</h2>
-				<div class="block bg-green-05">.bg-green-05</div>
-				<div
-					v-for="index in 9"
-					:key="index"
-					:class="[
-						'block',
-						`bg-green-${index}`,
-						index >= 5 ? 'text-white' : '',
-					]"
-				>
-					{{ `.bg-green-${index}` }}
-				</div>
 			</develop-box>
 		</develop-section>
 
 		<develop-section title="Button">
 			<develop-box title="Sizes">
-				<h2 class="title-6 subtitle">Large</h2>
+				<h2 class="heading heading--6 subtitle">Large</h2>
 				<v-row>
 					<v-col cols="auto">
 						<v-button
@@ -128,7 +46,7 @@
 				<br />
 				<br />
 
-				<h2 class="title-6 subtitle">Medium</h2>
+				<h2 class="heading heading--6 subtitle">Medium</h2>
 				<v-row>
 					<v-col cols="auto">
 						<v-button
@@ -159,7 +77,7 @@
 				<br />
 				<br />
 
-				<h2 class="title-6 subtitle">Small</h2>
+				<h2 class="heading heading--6 subtitle">Small</h2>
 				<v-row>
 					<v-col cols="auto">
 						<v-button
@@ -189,7 +107,9 @@
 			</develop-box>
 
 			<develop-box title="Icons">
-				<h2 class="title-6 subtitle">Icon and Text Buttons</h2>
+				<h2 class="heading heading--6 subtitle">
+					Icon and Text Buttons
+				</h2>
 				<v-row items="center">
 					<v-col cols="auto">
 						<v-button
@@ -231,7 +151,7 @@
 				<br />
 				<br />
 
-				<h2 class="title-6 subtitle">Icon Buttons</h2>
+				<h2 class="heading heading--6 subtitle">Icon Buttons</h2>
 				<v-row>
 					<v-col cols="auto">
 						<v-button color="ghost" :icon="{ name: 'icon-eye' }" />
